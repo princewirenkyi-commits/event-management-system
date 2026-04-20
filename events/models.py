c96fa19df586b39831f4dca52fba0c7da89e5668
M1ss1ngL1nx <ujc33101@gmail.com>
2026-04-20 12:46:12 +0000

    feat: Add category field to Event model with predefined choices

--- events/models.py
+++ events/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
+    
+    CategoryChoices = [
+        ("Conference","conference"),
+        ("Workshop","workshop"), 
+        ("Webinar","webinar"), 
+        ("Networking","networking"), 
+        ("Seminar","seminar")
+        ]
+    
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField(max_length=500)
+    category = models.CharField(max_length=50, choices=CategoryChoices)
+    
