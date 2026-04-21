82acb5276aa83eb2556b757089f34dbddbb6a52b
Prince Kofi Wirenkyi <princewirenkyi@gmail.com>
2026-04-21 15:05:21 +0000

    remove redundant fields from Event model

--- events/models.py
+++ events/models.py
@@ -68,10 +68,6 @@ class Event(models.Model):
     class Meta:
          ordering = ['start_datetime']
 
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField(max_length=500)
-    category = models.CharField(max_length=50, choices=CategoryChoices)
     
     
     
