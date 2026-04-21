2d632687087fa95cf8ade6fb5e1dbfc008d57c77
M1ss1ngL1nx <ujc33101@gmail.com>
2026-04-21 18:29:47 +0000

    feat: Define Attendee model with necessary fields

--- attendees/models.py
+++ attendees/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Attendee(models.Model):
+    attendee_id = models.CharField(max_length=10, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+    company = models.CharField(max_length=200, blank=True)
+    job_title = models.CharField(max_length=100, blank=True)
+    date_registered = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    
+    #-----Methods------
+    
